b2768452ea8596fb5cad95c2f6b93baba4e4dadf
Kefest1 <99877119+Kefest1@users.noreply.github.com>
2023-06-13 10:56:31 +0200

    Update server.js

--- source/server.js
+++ source/server.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Knowit')
+})
+
+app.get('/test', (req, res) => {
+    res.send('Test Passsed')
 })
 
 app.listen(4080)
